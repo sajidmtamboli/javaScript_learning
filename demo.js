76afe394174c50d3d235ae458e496ce3e57b03f7
sajidmtamboli <sajidtamboli1294@gmail.com>
2022-10-04 08:32:29 +0530

    demo

---
+++ demo.js
@@ -0,0 +1,5 @@
+console.log("Hello");
+console.log("My name is sajid");
+console.info("This is Info"); //info means information
+console.warn("I am not bad guy"); //warn means warning
+console.error("Oop failed to excute");
