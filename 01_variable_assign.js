d3f06b9c169d39694151d80a63b0da31636daaa8
sajidmtamboli <sajidtamboli1294@gmail.com>
2022-10-06 19:08:55 +0530

    new assigment

---
+++ 01_variable_assign.js
@@ -0,0 +1,17 @@
+var my_full_name;
+console.log(my_full_name);
+my_full_name="Sajid  Tamboli"
+console.log(my_full_name);
+my_full_name="Sajid Mahammad Tamboli"
+console.log(my_full_name);
+var myLove;
+myLove="I Love only JS"
+console.log(myLove);
+var firstName="Sajid";
+var lastName="Tamboli";
+var age="28";
+var address="kole";
+var pin_code="413314";
+var gender="Male";
+var is_married="Yes"
+console.log(firstName,lastName,age,address,pin_code,gender,is_married);
