9a90a7431c8f021f4784c4b3aacf48c56063aeda
sajidmtamboli <sajidtamboli1294@gmail.com>
2022-10-07 22:31:53 +0530

    change

---
+++ 02_assigmentData.js
@@ -0,0 +1,9 @@
+var pin=413314;
+var city="kole";
+var is_married=true;
+console.log("pin :", pin, "type :", typeof pin);
+console.log("city:",city, "type",typeof city);
+console.log("is_married:",is_married, "type",typeof is_married);
+console.log("==================================");
+console.log("Diffrence between Null & Undefined");
+
