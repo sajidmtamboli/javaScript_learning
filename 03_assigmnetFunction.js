7559f9e1ac46c3194986c7f104b0a50f0b20f834
sajidmtamboli <sajidtamboli1294@gmail.com>
2022-10-08 08:24:45 +0530

    assigment

---
+++ 03_assigmnetFunction.js
@@ -0,0 +1,30 @@
+console.log("==============================");
+function info() {
+   console.log("I am learing Java script");
+   console.log("I am also learn HTML & CSS");
+}
+info();
+console.log("==============================");
+function name1(a1,a2) {
+    console.log(a1,a2);
+}
+name1("sajid","Tamboli")
+console.log("==============================");
+function swap_values(arg1,arg2) {
+    console.log("=====================Before swap=======================");
+    console.log(arg1,arg2);
+    console.log("=====================After swap=======================");
+    var temp=arg1;
+    arg1=arg2;
+    arg2=temp; 
+    console.log(arg1,arg2);
+} swap_values("Virat","Anushka")
+swap_values(1000, 2000)
+console.log("==============================");
+ function add_three_numbers(num1,num2,num3) {
+    console.log(num1+num2+num3);  
+ }add_three_numbers(10.23, 600, 40)
+ add_three_numbers("hello","good" ,"morning")
+
+
+
