ded0a51767ddb1aa8797e3176e304c2227e892a1
sajidmtamboli <sajidtamboli1294@gmail.com>
2022-10-10 19:13:32 +0530

    change

---
+++ 04_string.js
@@ -0,0 +1,15 @@
+var greet = "Good Morning";
+
+console.log(greet);
+
+var count = greet.length;
+console.log(count);
+
+console.log(greet.length);
+
+var sonali = greet.charAt(8);
+console.log(sonali);
+
+console.log(greet.charAt(8));
+
+greet.indexOf
