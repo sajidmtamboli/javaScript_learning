f4276e7aca79c0f98df13a42545519816a0a2f69
sajidmtamboli <sajidtamboli1294@gmail.com>
2022-10-12 20:15:28 +0530

    new file

---
+++ 05_opertorAss2.js
@@ -0,0 +1,103 @@
+var greterNumber = function(num1,num2){
+var result= num1 > num2 ? num1: num2;
+console.log(`Greter Number is:- ${result}`);
+
+}
+greterNumber(10,-10)
+
+var greterNumber = function(num1,num2){
+    var result= num1 > num2 ? num1: num2;
+    console.log(`Greter Number is:- ${result}`);
+    
+    }
+    greterNumber(800,899)
+    console.log("========================================");
+
+    var evenOrodd= function(num){
+        var result= num%2==0 ? "even" : "odd";
+        console.log(`The given no 29 is:-${result}`);
+        
+    }
+    evenOrodd(29)
+
+    var evenOrodd= function(num){
+        var result= num%2==0 ? "even" : "odd";
+        console.log(`The given no 44 is:-${result}`);
+        
+    }
+    evenOrodd(44)
+
+    var evenOrodd= function(num){
+        var result= num%2==0 ? "even" : "odd";
+        console.log(`The given no 0 is:-${result}`);
+        
+    }
+    evenOrodd(0)
+
+    var evenOrodd= function(num){
+        var result= num%2==0 ? "even" : "odd";
+        console.log(`The given no 101 is:-${result}`);
+        
+    }
+    evenOrodd(101)
+    console.log("========================================");
+
+    var evenOrodd= function(num){
+        var result= num%2==0 ? true : false;
+        console.log(`The given no 29 is:-${result}`);
+        
+    }
+    evenOrodd(29)
+
+    var evenOrodd= function(num){
+        var result= num%2==0 ? true : false;
+        console.log(`The given no 44 is:-${result}`);
+        
+    }
+    evenOrodd(44)
+
+    var evenOrodd= function(num){
+        var result= num%2==0 ? true : false;
+        console.log(`The given no 0 is:-${result}`);
+        
+    }
+    evenOrodd(0)
+
+    var evenOrodd= function(num){
+        var result= num%2==0 ? true : false;
+        console.log(`The given no 101 is:-${result}`);
+        
+    }
+    evenOrodd(101)
+
+    console.log("========================================");
+
+    var evenOrodd= function() {
+    var string="JavaScript"
+    console.log(`${string}`);
+    var length=string.length
+    console.log(`Length of string=${length}`);
+    var result= length%2==0? "Even" : "Odd"
+    console.log(`given word is odd or even= ${result}`);
+    }
+    evenOrodd()
+    console.log("---------------------------------------");
+    var evenOrodd= function() {
+        var string="Developer"
+        console.log(`${string}`);
+        var length=string.length
+        console.log(`Length of string=${length}`);
+        var result= length%2==0? "Even" : "Odd"
+        console.log(`given word is odd or even= ${result}`);
+        }
+        evenOrodd()
+        console.log("---------------------------------------");
+        var evenOrodd= function() {
+            var string="Goggle"
+            console.log(`${string}`);
+            var length=string.length
+            console.log(`Length of string=${length}`);
+            var result= length%2==0? "Even" : "Odd"
+            console.log(`given word is odd or even= ${result}`);
+            }
+            evenOrodd()
---
+++ 05_opertorAssig1.js
@@ -0,0 +1,55 @@
+var wordLengthSqaure = function (word) {
+    var string = "JavaScript"
+    console.log(`word is:- ${string}`);
+    var length = string.length
+    console.log(`The length of word is:- ${length}`);
+    return length * length
+
+}
+var result = wordLengthSqaure("javaScript");
+console.log(`Total word length square is:-${result}`);
+
+console.log("======================================");
+
+var wordLengthSqaure = function (word) {
+    var string = "Angular"
+    console.log(`word is:- ${string}`);
+    var length = string.length
+    console.log(`The length of word is:- ${length}`);
+    return length * length
+
+}
+var output = wordLengthSqaure("Angular");
+console.log(`Total word length square is:-${output}`);
+console.log("======================================");
+
+var wordLengthSqaure = function (word) {
+    var string = "Developer"
+    console.log(`word is:- ${string}`);
+    var length = string.length
+    console.log(`The length of word is:- ${length}`);
+    return length * length
+}
+var answer=wordLengthSqaure("Developer");
+console.log(`Total word length square is:- ${answer}`);
+console.log("======================================");
+
+var statment=function(){
+    var string="I am Angular developer"
+    console.log(`The given string is:- ${string}`);
+    var length=string.length
+    console.log(`The string length is:- ${length}`);
+    console.log(`The reverse of string is:- ${string.split(" ").reverse()}`);
+    var splitResult=string.split(" ");
+    console.log(`string:- ${splitResult}`);
+    console.log(`Total String words is:- ${splitResult.length}`);
+    var leng1=22;
+    var leng2=4;
+    var divide=leng1/leng2;
+    console.log(`String length divided by total no of words:- ${divide}`);
+    console.log(`String length multiply by total no of word:- ${leng1*leng2}`);
+
+}
+statment()
+    
+
