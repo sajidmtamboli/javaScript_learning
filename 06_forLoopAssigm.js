912efa069fe0997251e8407e2f39cd7841ea83da
sajidmtamboli <sajidtamboli1294@gmail.com>
2022-10-14 20:44:43 +0530

    assigment

---
+++ 06_forLoopAssigm.js
@@ -0,0 +1,43 @@
+for (let index = 5; index <=15; index++) {
+    console.log(index);
+    
+}
+console.log("-----------------");
+
+for (let index = 50; index >=40; index--) {
+    console.log(index);
+    
+}
+console.log("-----------------");
+for (let index = 1; index < 30; index++) {
+    if(index%2!=0){
+        console.log(index);
+    }
+    
+}
+console.log("-----------------");
+for (let index = 1; index < 12; index++) {
+    if(index%2==0){
+        console.log(index);
+    }
+    
+}
+console.log("-----------------");
+
+for (let index = 5; index <= 50;  index=index+5) {
+    
+    console.log(index);
+}
+console.log("-----------------");
+for (let index = 10; index <= 100;  index=index+10) {
+    
+    console.log(index);
+}
+console.log("-----------------");
+for (let index = 100; index >= 10;  index=index-10) {
+    
+    console.log(index);
+}
+
+
+
---
+++ 07_switchCaseAssig.js
@@ -0,0 +1,40 @@
+var month= 1;
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+
+    case 2:
+        console.log("Febaruy");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("Jully");
+        break;
+    case 7:
+        console.log("Augst");
+        break;
+    case 7:
+        console.log("September");
+        break;
+    case 7:
+        console.log("Octumber");
+        break;
+    case 7:
+        console.log("November");
+        break;
+    case 7:
+        console.log("December");
+        break;
+}
